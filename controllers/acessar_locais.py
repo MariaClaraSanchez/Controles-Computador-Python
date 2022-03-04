24cd5963c9e1ba7cff1249613222522e147cd4b2
Maria Sanchez <clarasanchez2398@gmail.com>
2022-03-04 15:16:18 -0300

    Reajustes finais, documentei todo o código

--- controllers/acessar_locais.py
+++ controllers/acessar_locais.py
@@ -1,9 +1,13 @@
-from turtle import pos
 import pyautogui
 import pyautogui as posicaoMouse
 
 class Locais:
     def __init__(self,opcao_escolhida:str) -> None:
+        """_summary_
+            Responsável por direcionar para o aplicativo escolhido 
+        Args:
+            opcao_escolhida (str): recebe o aplicativo que o usuário recebeu
+        """
         self.pesquisa = posicaoMouse.moveTo(x=327, y=1052)
         self.click_pesquisa = posicaoMouse.click(x=327, y=1052)  
         self.time = posicaoMouse.sleep(3)
@@ -11,6 +15,13 @@ class Locais:
         self.enter = posicaoMouse.press('enter')
 
     def escolha(self) -> None:
+        """
+        Chama as varivaeis responsáveis por posicionar o mouse na barra de pesquisa;
+        Dar um click na barra de pesquisa;
+        Dar um tempo de espera para dar tempo de inserir o texto completamente na barra de pesquisa;
+        Insere o texto;
+        Após inserir o texto é dado um enter e aplicação abre.
+        """
         self.pesquisa 
         self.click_pesquisa
         self.time
--- controllers/tela_opcoes.py
+++ controllers/tela_opcoes.py
@@ -4,8 +4,14 @@ import pyautogui as escolha_opcao
 class Tela:
     def __init__(self) -> None:
         pass
-
     def opcao_user(self) -> str:
+        """Responsável por incluir o nome dos aplicativos a serem abertos,
+        e também por criar uma janela na tela, com botões para que o usuário 
+        consiga clicar.
+
+        Returns:
+            opcao: retorna uma string com a opção escolhida pelo usuário
+        """
         opcao = pyautogui.confirm('Clique no aplicativo que deseja abrir', 
                     buttons = ['Discord', 'Microsoft Teams','Outlook','Visual Studio Code'
                     'Google Chrome','PowerPoint','Excel','Word',
